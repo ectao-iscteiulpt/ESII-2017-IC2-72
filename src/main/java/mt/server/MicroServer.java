322ea296d57dd6d93150ba360072d0c62c9305a6
Rui Brito <raccb@iscte-iul.pt>
2017-05-08 23:50:05 +0100

    Erro xml

--- src/main/java/mt/server/MicroServer.java
+++ src/main/java/mt/server/MicroServer.java
@@ -1,5 +1,7 @@
 package mt.server;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -14,6 +16,17 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.swing.JOptionPane;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 import mt.Order;
 import mt.comm.ServerComm;
@@ -23,14 +36,14 @@ import mt.exception.ServerException;
 import mt.filter.AnalyticsFilter;
 
 /**
- * MicroTraderServer implementation. This class should be responsible
- * to do the business logic of stock transactions between buyers and sellers.
+ * MicroTraderServer implementation. This class should be responsible to do the
+ * business logic of stock transactions between buyers and sellers.
  * 
  * @author Group 78
  *
  */
 public class MicroServer implements MicroTraderServer {
-	
+
 	public static void main(String[] args) {
 		ServerComm serverComm = new AnalyticsFilter(new ServerCommImpl());
 		MicroTraderServer server = new MicroServer();
@@ -58,7 +71,7 @@ public class MicroServer implements MicroTraderServer {
 	 * Order Server ID
 	 */
 	private static int id = 1;
-	
+
 	/** The value is {@value #EMPTY} */
 	public static final int EMPTY = 0;
 
@@ -74,7 +87,7 @@ public class MicroServer implements MicroTraderServer {
 	@Override
 	public void start(ServerComm serverComm) {
 		serverComm.start();
-		
+
 		LOGGER.log(Level.INFO, "Starting Server...");
 
 		this.serverComm = serverComm;
@@ -82,103 +95,105 @@ public class MicroServer implements MicroTraderServer {
 		ServerSideMessage msg = null;
 		while ((msg = serverComm.getNextMessage()) != null) {
 			ServerSideMessage.Type type = msg.getType();
-			
-			if(type == null){
+
+			if (type == null) {
 				serverComm.sendError(null, "Type was not recognized");
 				continue;
 			}
 
 			switch (type) {
-				case CONNECTED:
-					try{
-						processUserConnected(msg);
-					}catch (ServerException e) {
-						serverComm.sendError(msg.getSenderNickname(), e.getMessage());
-					}
-					break;
-				case DISCONNECTED:
-					processUserDisconnected(msg);
-					break;
-				case NEW_ORDER:
-					try {
-						// Boolean variable for the Business rules
-						boolean br3 = true;
-						
-						verifyUserConnected(msg);
-						
-						// Set of orders to check Business Rules
-						Set<Order> orders = orderMap.get(msg.getOrder().getNickname());
-						
-						// Verification of msg quantity for BR3
-						if (msg.getOrder().getNumberOfUnits() < 10) {
-							JOptionPane.showMessageDialog(null, "You cannot make a order with a quantity below 10 units.");
-							br3 = false;
-						}
-						
-						if (br3 == true) {
-							if(msg.getOrder().getServerOrderID() == EMPTY){
-								msg.getOrder().setServerOrderID(id++);
-								notifyAllClients(msg.getOrder());
-								processNewOrder(msg);
-							}
-						}
-					} catch (ServerException e) {
-						serverComm.sendError(msg.getSenderNickname(), e.getMessage());
-					}
-					break;
-				default:
-					break;
+			case CONNECTED:
+				try {
+					processUserConnected(msg);
+				} catch (ServerException e) {
+					serverComm.sendError(msg.getSenderNickname(), e.getMessage());
 				}
+				break;
+			case DISCONNECTED:
+				processUserDisconnected(msg);
+				break;
+			case NEW_ORDER:
+				try {
+					// Boolean variable for the Business rules
+					boolean br3 = true;
+
+					verifyUserConnected(msg);
+
+					// Set of orders to check Business Rules
+					Set<Order> orders = orderMap.get(msg.getOrder().getNickname());
+
+					// Verification of msg quantity for BR3
+					if (msg.getOrder().getNumberOfUnits() < 10) {
+						JOptionPane.showMessageDialog(null, "You cannot make a order with a quantity below 10 units.");
+						br3 = false;
+					}
+
+					if (br3 == true) {
+						if (msg.getOrder().getServerOrderID() == EMPTY) {
+							msg.getOrder().setServerOrderID(id++);
+							notifyAllClients(msg.getOrder());
+							processNewOrder(msg);
+						}
+					}
+				} catch (ServerException e) {
+					serverComm.sendError(msg.getSenderNickname(), e.getMessage());
+				}
+				break;
+			default:
+				break;
+			}
 		}
 		LOGGER.log(Level.INFO, "Shutting Down Server...");
 	}
-
 
 	/**
 	 * Verify if user is already connected
 	 * 
 	 * @param msg
-	 * 			the message sent by the client
+	 *            the message sent by the client
 	 * @throws ServerException
-	 * 			exception thrown by the server indicating that the user is not connected
+	 *             exception thrown by the server indicating that the user is
+	 *             not connected
 	 */
 	private void verifyUserConnected(ServerSideMessage msg) throws ServerException {
 		for (Entry<String, Set<Order>> entry : orderMap.entrySet()) {
-			if(entry.getKey().equals(msg.getSenderNickname())){
+			if (entry.getKey().equals(msg.getSenderNickname())) {
 				return;
 			}
 		}
 		throw new ServerException("The user " + msg.getSenderNickname() + " is not connected.");
-		
+
 	}
 
 	/**
 	 * Process the user connection
 	 * 
 	 * @param msg
-	 * 			  the message sent by the client
+	 *            the message sent by the client
 	 * 
 	 * @throws ServerException
-	 * 			exception thrown by the server indicating that the user is already connected
+	 *             exception thrown by the server indicating that the user is
+	 *             already connected
 	 */
 	private void processUserConnected(ServerSideMessage msg) throws ServerException {
 		LOGGER.log(Level.INFO, "Connecting client " + msg.getSenderNickname() + "...");
-		
+
 		// verify if user is already connected
 		for (Entry<String, Set<Order>> entry : orderMap.entrySet()) {
-			if(entry.getKey().equals(msg.getSenderNickname())){
+			if (entry.getKey().equals(msg.getSenderNickname())) {
 				throw new ServerException("The user " + msg.getSenderNickname() + " is already connected.");
 			}
 		}
-		
+
 		// register the new user
 		orderMap.put(msg.getSenderNickname(), new HashSet<Order>());
-		
+
 		notifyClientsOfCurrentActiveOrders(msg);
 	}
-	
+
 	/**
 	 * Send current active orders sorted by server ID ASC
+	 * 
 	 * @param msg
 	 */
 	private void notifyClientsOfCurrentActiveOrders(ServerSideMessage msg) {
@@ -190,7 +205,7 @@ public class MicroServer implements MicroTraderServer {
 				ordersToSend.add(order);
 			}
 		}
-		
+
 		// sort the orders to send to clients by server id
 		Collections.sort(ordersToSend, new Comparator<Order>() {
 			@Override
@@ -198,8 +213,8 @@ public class MicroServer implements MicroTraderServer {
 				return o1.getServerOrderID() < o2.getServerOrderID() ? -1 : 1;
 			}
 		});
-		
-		for(Order order : ordersToSend){
+
+		for (Order order : ordersToSend) {
 			serverComm.sendOrder(msg.getSenderNickname(), order);
 		}
 	}
@@ -208,14 +223,14 @@ public class MicroServer implements MicroTraderServer {
 	 * Process the user disconnection
 	 * 
 	 * @param msg
-	 * 			  the message sent by the client
+	 *            the message sent by the client
 	 */
 	private void processUserDisconnected(ServerSideMessage msg) {
-		LOGGER.log(Level.INFO, "Disconnecting client " + msg.getSenderNickname()+ "...");
-		
-		//remove the client orders
+		LOGGER.log(Level.INFO, "Disconnecting client " + msg.getSenderNickname() + "...");
+
+		// remove the client orders
 		orderMap.remove(msg.getSenderNickname());
-		
+
 		// notify all clients of current unfulfilled orders
 		for (Entry<String, Set<Order>> entry : orderMap.entrySet()) {
 			Set<Order> orders = entry.getValue();
@@ -235,15 +250,17 @@ public class MicroServer implements MicroTraderServer {
 		LOGGER.log(Level.INFO, "Processing new order...");
 
 		Order o = msg.getOrder();
-		
+
 		// save the order on map
 		saveOrder(o);
+
+		functionalRequirementXML(msg, null, 2);
 
 		// if is buy order
 		if (o.isBuyOrder()) {
 			processBuy(msg.getOrder());
 		}
-		
+
 		// if is sell order
 		if (o.isSellOrder()) {
 			processSell(msg.getOrder());
@@ -259,52 +276,56 @@ public class MicroServer implements MicroTraderServer {
 		updatedOrders = new HashSet<>();
 
 	}
-	
+
 	/**
 	 * Store the order on map
 	 * 
 	 * @param o
-	 * 			the order to be stored on map
+	 *            the order to be stored on map
 	 */
 	private void saveOrder(Order o) {
 		LOGGER.log(Level.INFO, "Storing the new order...");
-		
-		//save order on map
+
+		// save order on map
 		Set<Order> orders = orderMap.get(o.getNickname());
-		orders.add(o);		
+		orders.add(o);
 	}
 
 	/**
 	 * Process the sell order
 	 * 
 	 * @param sellOrder
-	 * 		Order sent by the client with a number of units of a stock and the price per unit he wants to sell
+	 *            Order sent by the client with a number of units of a stock and
+	 *            the price per unit he wants to sell
 	 */
-	private void processSell(Order sellOrder){
+	private void processSell(Order sellOrder) {
 		LOGGER.log(Level.INFO, "Processing sell order...");
-		
+
 		for (Entry<String, Set<Order>> entry : orderMap.entrySet()) {
 			for (Order o : entry.getValue()) {
-				if (o.isBuyOrder() && o.getStock().equals(sellOrder.getStock()) && o.getPricePerUnit() >= sellOrder.getPricePerUnit()) {
-					doTransaction (o, sellOrder);
+				if (o.isBuyOrder() && o.getStock().equals(sellOrder.getStock())
+						&& o.getPricePerUnit() >= sellOrder.getPricePerUnit()) {
+					doTransaction(o, sellOrder);
 				}
 			}
 		}
-		
+
 	}
-	
+
 	/**
 	 * Process the buy order
 	 * 
 	 * @param buyOrder
-	 *          Order sent by the client with a number of units of a stock and the price per unit he wants to buy
+	 *            Order sent by the client with a number of units of a stock and
+	 *            the price per unit he wants to buy
 	 */
 	private void processBuy(Order buyOrder) {
 		LOGGER.log(Level.INFO, "Processing buy order...");
 
 		for (Entry<String, Set<Order>> entry : orderMap.entrySet()) {
 			for (Order o : entry.getValue()) {
-				if (o.isSellOrder() && buyOrder.getStock().equals(o.getStock()) && o.getPricePerUnit() <= buyOrder.getPricePerUnit()) {
+				if (o.isSellOrder() && buyOrder.getStock().equals(o.getStock())
+						&& o.getPricePerUnit() <= buyOrder.getPricePerUnit()) {
 					doTransaction(buyOrder, o);
 				}
 			}
@@ -315,62 +336,69 @@ public class MicroServer implements MicroTraderServer {
 	/**
 	 * Process the transaction between buyer and seller
 	 * 
-	 * @param buyOrder 		Order sent by the client with a number of units of a stock and the price per unit he wants to buy 
-	 * @param sellerOrder	Order sent by the client with a number of units of a stock and the price per unit he wants to sell
+	 * @param buyOrder
+	 *            Order sent by the client with a number of units of a stock and
+	 *            the price per unit he wants to buy
+	 * @param sellerOrder
+	 *            Order sent by the client with a number of units of a stock and
+	 *            the price per unit he wants to sell
 	 */
 	private void doTransaction(Order buyOrder, Order sellerOrder) {
 		LOGGER.log(Level.INFO, "Processing transaction between seller and buyer...");
 
 		if (buyOrder.getNumberOfUnits() >= sellerOrder.getNumberOfUnits()) {
-			buyOrder.setNumberOfUnits(buyOrder.getNumberOfUnits()
-					- sellerOrder.getNumberOfUnits());
+			buyOrder.setNumberOfUnits(buyOrder.getNumberOfUnits() - sellerOrder.getNumberOfUnits());
 			sellerOrder.setNumberOfUnits(EMPTY);
 		} else {
-			sellerOrder.setNumberOfUnits(sellerOrder.getNumberOfUnits()
-					- buyOrder.getNumberOfUnits());
+			sellerOrder.setNumberOfUnits(sellerOrder.getNumberOfUnits() - buyOrder.getNumberOfUnits());
 			buyOrder.setNumberOfUnits(EMPTY);
 		}
-		
+
 		updatedOrders.add(buyOrder);
 		updatedOrders.add(sellerOrder);
+
+		
 	}
-	
+
 	/**
 	 * Notifies clients about a changed order
 	 * 
 	 * @throws ServerException
-	 * 			exception thrown in the method notifyAllClients, in case there's no order
+	 *             exception thrown in the method notifyAllClients, in case
+	 *             there's no order
 	 */
 	private void notifyClientsOfChangedOrders() throws ServerException {
 		LOGGER.log(Level.INFO, "Notifying client about the changed order...");
-		for (Order order : updatedOrders){
+		for (Order order : updatedOrders) {
 			notifyAllClients(order);
 		}
 	}
-	
+
 	/**
 	 * Notifies all clients about a new order
 	 * 
-	 * @param order refers to a client buy order or a sell order
+	 * @param order
+	 *            refers to a client buy order or a sell order
 	 * @throws ServerException
-	 * 				exception thrown by the server indicating that there is no order
-	 */			
+	 *             exception thrown by the server indicating that there is no
+	 *             order
+	 */
 	private void notifyAllClients(Order order) throws ServerException {
 		LOGGER.log(Level.INFO, "Notifying clients about the new order...");
-		if(order == null){
+		if (order == null) {
 			throw new ServerException("There was no order in the message");
 		}
 		for (Entry<String, Set<Order>> entry : orderMap.entrySet()) {
-			serverComm.sendOrder(entry.getKey(), order); 
+			serverComm.sendOrder(entry.getKey(), order);
 		}
 	}
-	
+
 	/**
 	 * Remove fulfilled orders
 	 */
 	private void removeFulfilledOrders() {
 		LOGGER.log(Level.INFO, "Removing fulfilled orders...");
-		
+
 		// remove fulfilled orders
 		for (Entry<String, Set<Order>> entry : orderMap.entrySet()) {
 			Iterator<Order> it = entry.getValue().iterator();
@@ -381,6 +409,55 @@ public class MicroServer implements MicroTraderServer {
 				}
 			}
 		}
+	}
+
+	private void functionalRequirementXML(ServerSideMessage msg, Order order, int type) {
+		// type 1 is an order and type 2 is a message
+		try {
+			System.out.println("AQUIIIIIIIIIIIIIIIIIIII!!!!!!!!!!!!!");
+			File inputFile = new File("persistence.xml");
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(inputFile);
+			doc.getDocumentElement().normalize();
+			Element newElementOrder = doc.createElement("Order");
+
+			if (type == 2) {
+
+				newElementOrder.setAttribute("Id", Integer.toString(msg.getOrder().getServerOrderID()));
+				if (msg.getOrder().isBuyOrder())
+					newElementOrder.setAttribute("Type", "Buy");
+				else
+					newElementOrder.setAttribute("Type", "Sell");
+				newElementOrder.setAttribute("Stock", msg.getOrder().getStock());
+				newElementOrder.setAttribute("Units", Integer.toString(msg.getOrder().getNumberOfUnits()));
+				newElementOrder.setAttribute("Price", Double.toString(msg.getOrder().getPricePerUnit()));
+
+				// Create new element Customer
+				Element newElementCustomer = doc.createElement("Customer");
+				newElementCustomer.setTextContent(msg.getSenderNickname());
+				newElementOrder.appendChild(newElementCustomer);
+			} else {
+
+			}
+
+			// Add new node to XML document root element
+			System.out.println("----- Adding new element to root element -----");
+			System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+			Node n = doc.getDocumentElement();
+			n.appendChild(newElementOrder);
+			// Save XML document
+			System.out.println("Save XML document.");
+			Transformer transformer = TransformerFactory.newInstance().newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			StreamResult result = new StreamResult(new FileOutputStream("persistence.xml"));
+			DOMSource source = new DOMSource(doc);
+			transformer.transform(source, result);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 }
